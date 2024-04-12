79e41c8dd55c856158b21a1cf8e381f0133a9977
tamadNaBrowNie <shawn_aberin@dlsu.edu.ph>
2024-04-13 00:51:03 +0800

    fixed unreferenced bug

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { app, DB_URI } = require("./app");
+const { app } = require("./app");
 
 const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
@@ -8,7 +8,7 @@ mongoose
   .connect(
     "mongodb://rootFinal:rootroot@final-db-cluster.cluster-cpuaqu8wsgkj.ap-southeast-2.docdb.amazonaws.com:27017/?tls=true&retryWrites=false&directConnection=true",
     {
-      tlsCAFile: `global-bundle.pem`,
+      tlsCAFile: "global-bundle.pem",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
